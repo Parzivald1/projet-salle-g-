0d78dd4e839ad61fa3aeaad971e317b44ba7f787
Parzivald1 <tmagnin7@outlook.fr>
2026-04-15 15:08:30 +0200

    Delete firewall_ultra directory

--- firewall_ultra/api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Module API
-Communication avec OPNsense et gestion des règles firewall
-"""
-
-from .opnsense import opnsense, OPNsenseAPI
-from .firewall_rules import firewall_manager, FirewallManager
